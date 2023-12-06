dfee743334b1fcfea5514b421c48927383e04b75
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2023-12-06 12:34:47 +0100

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/earthquake-data-vis/",
+  base: "/earthquake_data_vis/",
   build: {
     outDir: "./build",
   },
